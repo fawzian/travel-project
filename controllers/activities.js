07186f7cd2223ab60dabce9d3bf1c5e36c46d3b0
fawzian <fawzianur@gmail.com>
2021-04-15 21:46:48 -0400

    edit and update routes are working

--- controllers/activities.js
+++ controllers/activities.js
@@ -18,8 +18,22 @@ function destroy(req, res) {
 }
 
 function update(req, res) {
-    res.send("update")
+  if(req.body.booked === 'on') {
+      req.body.booked = 'true'
+  } else {
+      req.body.booked = 'false'
+  }
+  Activity.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedModel) => (res.redirect('/activities')))
 }
+
+// if (req.body.readyToEat === 'on') {
+//     req.body.readyToEat = 'true'
+// } else {
+//     req.body.readyToEat = 'false'
+// }
+// // res.send(req.body)
+// Fruit.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedModel) => (res.redirect('/fruits')))
+// })
 
 function create(req, res) {
     if (req.body.booked === "on") {
@@ -35,8 +49,14 @@ function create(req, res) {
     }
 
 function edit(req, res) {
-    res.send("edit")
+    Activity.findById(req.params.id, (err, foundActivity) => {
+        console.log("foundActivity", foundActivity);
+        res.render("activities/edit", {
+            activity: foundActivity
+        })
+    })
 }
+
 
 function show(req, res) {
     res.send("show")
