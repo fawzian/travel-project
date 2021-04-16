91f7b89ec945ac2dedca082325797af7850fbba9
fawzian <fawzianur@gmail.com>
2021-04-15 20:33:30 -0400

    restful routes created, but not rendering ejs views yet

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,13 +1,53 @@
 const Activity = require("../models/activity.js");
 
 function index(req, res) {
-    Activity.find({}, (error, allActivities) => {
-        activities: allActivities
-    })
+    Fruit.find({}, (error, allActivities) => {
+        res.render('index', {
+            activity: allActivities
+        });
+    });
+
+
+function newActivity(req, res) {
+    res.render("activities/new.ejs")
+}
+
+function destroy(req, res) {
+    res.send("destroy")
+}
+
+function update(req, res) {
+    res.send("update")
+}
+
+function create(req, res) {
+    if (req.body.booked === "on") {
+        req.body.booked = true;
+      } else {
+        req.body.booked = false;
+      }
+    
+      Activity.create(req.body, (error, createdActivity) => {
+        console.log(createdActivity);
+        res.redirect("/activities")
+      })
+    }
+
+function edit(req, res) {
+    res.send("edit")
+}
+
+function show(req, res) {
+    res.send("show")
 }
 
 
-
 module.exports = {
-    index
+    index, 
+    new: newActivity, 
+    delete: destroy,
+    update,
+    create,
+    edit,
+    show
 }
--- routes/activities.js
+++ routes/activities.js
@@ -2,7 +2,29 @@ const express = require("express");
 const router = express.Router();
 const activitiesCtrl = require('../controllers/activities')
 
-
+// index
 router.get('/', activitiesCtrl.index)
+
+
+// new
+router.get('/new', activitiesCtrl.new)
+
+
+// delete
+router.delete('/:id', activitiesCtrl.delete)
+
+// update
+router.put("/:id", activitiesCtrl.update)
+
+// create
+router.post("/", activitiesCtrl.create)
+
+
+// edit
+
+router.get('/:id/edit', activitiesCtrl.edit)
+
+// show 
+router.get('/:id', activitiesCtrl.show)
 
 module.exports = router
